3fec405f1867fc6ca498d14cbba71edbc5368dcb
forrest <zyyu1990@gmail.com>
2020-05-28 14:10:04 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# math-filecoinjs
-Sample code for Filecoin dApp API in MathWallet
+# For Math Wallet DAPP Developer
+
+## Using Math Wallet Filecoin JS API
+
+### Samples
+
+```
+
+// login
+// returns { "name": "test1", "address": "t1u3ynne5fllqpv4re5vkqbvowtsb2aggh2p4u5fa"}
+const account = await window.filecoin.login();
+
+// logout
+// returns {}
+await window.filecoin.logout();
+
+// Sign Transaction
+const transaction =  {
+        from: this.account.address,
+        gasLimit: 10000,
+        gasPrice: 1,
+        nonce: 0,
+        to: "t1uzmmfknk3pq5otq4wosqtzm3oq7675vb27qz6aq",
+        value: 1000000000000000000, // 精度18
+        method: 0,
+        params: ""
+      };
+await window.filecoin.requestSignature(transaction);
+
+```
+
+For details, please find the sample in this repo.
+
+### Download Math Wallet 麦子钱包下载
+
+[http://mathwallet.org](http://mathwallet.org)
+
+
---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <v-card light>
+          <v-card-title>{{account?account.name:"Open MathWallet"}}</v-card-title>
+          <v-card-subtitle>{{account?account.address:""}}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn class="primary" @click="login" v-if="!account">Log in</v-btn>
+            <v-btn class="warning" @click="logout" v-else>Log out</v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card light class="mt-2" v-if="account">
+          <v-card-title>Signature</v-card-title>
+          <v-card-subtitle>{{signature?JSON.stringify(signature):""}}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn class="success" @click="requestSignature">Signature</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      account: null,
+      signature: null
+    };
+  },
+  methods: {
+    logout() {
+      window.filecoin.getAccount().then(() => {
+        this.account = null;
+      });
+    },
+    login() {
+      if (!window.filecoin) {
+        alert("Please install MathWallet first!");
+        return;
+      }
+      window.filecoin.getAccount().then(account => {
+        this.account = account;
+      });
+    },
+    requestSignature() {
+      if (!this.account) {
+        alert("Please log in first!");
+        return;
+      }
+      let transaction = {
+        from: this.account.address,
+        gasLimit: 10000,
+        gasPrice: 1,
+        nonce: 0,
+        to: "t1uzmmfknk3pq5otq4wosqtzm3oq7675vb27qz6aq",
+        value: 1000000000000000000, // 精度18
+        method: 0,
+        params: ""
+      };
+      window.filecoin.requestSignature(transaction).then(signature => {
+        this.signature = signature;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
